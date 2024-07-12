8f6ca1f6279a01d4d826f7cbfdf433b45196ef5a
Franco Salvador Talarico <84205093+Rubik3x3@users.noreply.github.com>
2024-07-12 10:58:10 -0300

    DB

---
+++ TP_DB.sql
@@ -0,0 +1,16 @@
+USE empresa;
+DROP TABLE IF EXISTS empleados;
+CREATE TABLE empleados(
+	documento integer unsigned primary key,
+    nombre varchar(30) not null,
+    sexo char(1),
+    domicilio varchar(30),
+    localidad varchar(25),
+    telefono integer unsigned not null,
+    fechaingreso date not null,
+    fechanacimiento date not null,
+    sueldobasico dec(5,2) unsigned not null
+);
+
+INSERT INTO empleados (documento, nombre, sexo, domicilio, localidad, telefono, fechaingreso, fechanacimiento, sueldobasico) 
+values (22, "Pablo Rodriguez", 'm', "Galvan","Salta", 22, '2015-12-17','2015-12-17',200.50);
